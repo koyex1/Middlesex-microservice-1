584d270cd89b42aea982593df0951c88da80757c
Olumide Koyenikan <olumide.koyenikan@yahoo.com>
2023-08-16 20:45:28 +0100

    created all apis

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var express = require('express');
+var axios = require('axios');
 var bodyParser = require('body-parser')
 var apiConfig = express();
 var cors = require('cors');
@@ -8,28 +9,51 @@ apiConfig.use(bodyParser.json({limit: "50mb"}));
 apiConfig.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit: 5000}));
 
 
-var store = {};
+ApiFunction = async() =>{
 
-apiConfig.post('/get', async(req, res)=>{
-    const {z} = req.body;
-
+apiConfig.get('/getAll', async(req, res)=>{
+   // const {z} = req.body;
+   let data1 = await axios.get('http://localhost:3002/getAll')
+   console.log(data1.data)
+   res.json(data1.data)
 })
-   
+ 
+apiConfig.get('/get/:id', async(req, res)=>{
+   // const {z} = req.body;
+   const itemId = parseInt(req.params.id);
+   let data1 = await axios.get('http://localhost:3002/get/'+itemId)
+   console.log(data1.data)
+   res.json(data1.data)
+})
 
 apiConfig.post('/post', async(req, res)=>{
-    const {z} = req.body;
+   // const {z} = req.body;
+   let data1 = await axios.post('http://localhost:3002/post',req.body)
+   res.json(data1.data)
 
 })
 
 
-apiConfig.post('/update', async(req, res)=>{
-    const {z} = req.body;
-
+apiConfig.put('/update/:id', async(req, res)=>{
+   // const {z} = req.body;
+   const itemId = parseInt(req.params.id);
+   const updatedItem = req.body;
+   let data1 = await axios.put('http://localhost:3002/update/'+itemId,updatedItem)
+   res.json(data1.data)
 })
 
 
-apiConfig.post('/delete', async(req, res)=>{
-    const {z} = req.body;
-
+apiConfig.delete('/delete', async(req, res)=>{
+    //const {z} = req.body;
+    const itemId = parseInt(req.params.id);
+    let data1 = await axios.put('http://localhost:3002/delete/'+itemId)
+    res.json(data1.data)
 })
 
+}
+
+ApiFunction();
+
+http.Server(apiConfig).listen(3001, ()=>{
+    console.log("server running on 3001")
+})
